bbb9c3659b5cdcb683673259f575350475865743
TheShadolo <theshadolo@gmail.com>
2025-01-22 21:02:39 -0500

    Finished bookbot project

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ def main():
         file_contents = f.read()
     words = file_contents.split()
     word_count = len(words)
-    print(f"Word count: {word_count}")
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document.")
+    print()
     
     lower_string = file_contents.lower()
     char_count = {}
     for x in lower_string:
-        char_count[x] = char_count.get(x, 0) + 1
-    print(char_count)
+        if x.isalpha():
+            char_count[x] = char_count.get(x, 0) + 1
+        sorted_char_count = {key: value for key, value in 
+                             sorted(char_count.items(), 
+                                    key=lambda item: item[1], 
+                                    reverse=True)}
+    for x in sorted_char_count:
+        print(f"The '{x}' character was found {sorted_char_count[x]} times.")
+
 
 main()
